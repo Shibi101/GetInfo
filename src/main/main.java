a1d738bf70122b9d9ea5bd6bb4ed8a7c4f5e7978
Shibi101 <shihabhashimi@outlook.com>
2025-06-12 17:10:24 +0200

    added function to return all network adapters

--- src/main/main.java
+++ src/main/main.java
@@ -8,6 +8,7 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -22,15 +23,22 @@ public class main {
 	public static void main(String[] args) throws UnknownHostException, SocketException {
 		
 		InetAddress IAddress = InetAddress.getLocalHost();
+		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+		for(NetworkInterface netint : Collections.list(interfaces)) {
+			System.out.println(netint.getDisplayName());
+		}
 		
 		JFrame frame = new JFrame("IP adress");
 		JLabel OsName = new JLabel(("OS name: " + System.getProperty("os.name")));
 		JLabel OsVersion = new JLabel(("OS version: " + System.getProperty("os.version")));
 		JLabel OsArch = new JLabel(("OS architecture: " + System.getProperty("os.arch")));
-		JLabel IP = new JLabel("IP: " + IAddress.getHostAddress().trim().toString());
 		frame.setLayout(new GridLayout(5,2));
 		List<String> ipAddresses = getIpAddresses(!INCLUDE_LINK_LOCAL, !INCLUDE_LOOPBACK);
 		ipAddresses.forEach(System.out::println);
+		
+		String NetworkIp = ipAddresses.get(2);
+		
+		JLabel IP = new JLabel("ip address: " + NetworkIp);
 		
 		frame.add(OsName);
 		frame.add(OsVersion);
@@ -61,5 +69,13 @@ public class main {
 			}
 		}
 		return ipAddresses;
+	}
+	
+	public static String ExtractIp(List<String> ipAddresses) {
+		for(String ip : ipAddresses) {
+			return ip;
+		}
+		return null;
+		
 	}
 }
